431db31a5a19d679b6fbf78e8d194ad1f2f254b7
Sebastien Lerda <sebastien.lerda3d@gmail.com>
2020-10-30 16:03:51 +0100

    init branch

---
+++ writting_data.py
@@ -0,0 +1,18 @@
+# -*-coding:Utf-8 -*
+import json
+data = {}
+data["unlock_password"] = "1234"
+data["ubi"] = {"id": "mon_id","password": "password$£"}
+data["steam"] = {"id": "mon_id","password": "password"}
+with open('data.json', 'w') as outfile:
+    json.dump(data,outfile, ensure_ascii=False, indent=4)
+
+class Password_data:
+    def __init__(self):
+        self.data={}
+    
+    def add_new_account(self, account:str, id:str, password:str):
+        self.data[account] = {"id": id, "password": password}
+        
+    def edit_account(self, account:str, id:str= "", password:str=""):
+        
