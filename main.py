17f2ce396baef14e62d95eb05b1f2e9c7d9883bd
Sebastien Lerda <sebastien.lerda3d@gmail.com>
2020-11-04 11:18:51 +0100

    black formatting

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 # -*-coding:Utf-8 -*
 from tkinter import *
 
+
 class Interface(Frame):
-    
+
     """Notre fenêtre principale.
     Tous les widgets sont stockés comme attributs de cette fenêtre."""
-    
+
     def __init__(self, windows, **kwargs):
         # Top frame
         Frame.__init__(self, windows, width=1500, height=1100, **kwargs)
@@ -14,65 +15,96 @@ class Interface(Frame):
 
         self.logs = Label(self, text="")
         self.logs.pack()
-        
-        ## Body frame
-        self.frame_login = Frame(windows)
-        self.frame_login.pack()
-        self.message = Label(self.frame_login, text="Enter password:", justify="left",anchor=W)
-        self.message.pack(side="top",fill=X)
-        #### Child login frame
-        self.password_input = StringVar()
-        self.input_entry_password = Entry(self.frame_login, textvariable=self.password_input, width=30, show="*")
-        self.input_entry_password.pack()
-        self.connect_button = Button(self.frame_login, text="Connect", command=self.connect)
-        self.connect_button.pack(side="bottom")
-
+        self.display_login_frame()
         # Bottom Frame
         self.exit_button = Button(windows, text="Exit", command=self.quit)
         self.exit_button.pack(side="bottom")
+
+    def display_login_frame(self):
+        ## Body frame
+        self.frame_login = Frame(windows)
+        self.frame_login.pack()
+        self.message = Label(
+            self.frame_login, text="Enter password:", justify="left", anchor=W
+        )
+        self.message.pack(side="top", fill=X)
+        #### Child login frame
+        self.password_input = StringVar()
+        self.input_entry_password = Entry(
+            self.frame_login, textvariable=self.password_input, width=30, show="*"
+        )
+        self.input_entry_password.pack()
+        self.connect_button = Button(
+            self.frame_login, text="Connect", command=self.connect
+        )
+        self.connect_button.pack(side="bottom")
 
     def connect(self):
         input_pass = self.password_input.get()
         if self.is_password_valid(input_pass):
             self.logs["text"] = "You're logged."
             self.logs["fg"] = "black"
-            self.data_display()
+            self.display_data_frame()
         else:
             self.logs["text"] = "Wrong password."
             self.logs["fg"] = "red"
+
     def is_password_valid(self, password):
-        bdd_password= ""
+        bdd_password = ""
         if password == bdd_password:
             return True
         return False
-    def data_display(self):
-        
+
+    def display_data_frame(self):
+        # Create data display
         self.frame_login.destroy()
         self.frame_data = Frame(windows)
         self.frame_data.pack()
-        self.message = Label(self.frame_data, text="Enter the name of account:", justify="left",anchor=W)
-        self.message.pack(side="top",fill=X)
+        self.message = Label(
+            self.frame_data, text="Enter the name of account:", justify="left", anchor=W
+        )
+        self.message.pack(side="top", fill=X)
         self.account_name = StringVar()
-        self.entry_account = Entry(self.frame_data, textvariable=self.account_name, width=30)
+        self.entry_account = Entry(
+            self.frame_data, textvariable=self.account_name, width=30
+        )
         self.entry_account.pack()
         self.button_search = Button(self.frame_data, text="Search", command=self.search)
         self.button_search.pack(side="bottom")
 
         # Display data account
-        self.display_account_info()
+        self.display_account_info(self.frame_data)
 
-    def display_account_info(self):
-        account_infos = self.account_data()
+    def display_account_info(self, parent_frame):
+        account_infos = self.get_account_data()
         for element in account_infos:
-            print(element)
+            data_elem = account_infos[element]
+            data_id = "id: {0}".format(data_elem["id"])
+            data_pass = "password: {0}".format(data_elem["password"])
+            frame_element = Frame(parent_frame, bg="grey", bd=3)
+            frame_element.pack(fill=X)
 
-    def account_data(self):
+            account_name = Label(
+                frame_element, text=element, justify="left", anchor=W, font=(12)
+            )
+            account_name.pack(side="top", fill=X)
+            account_id = Label(frame_element, text=data_id, justify="left", anchor=W)
+            account_id.pack(side="top", fill=X)
+            account_password = Label(
+                frame_element, text=data_pass, justify="left", anchor=W
+            )
+            account_password.pack(side="top", fill=X)
+
+    def get_account_data(self):
         import json
-        with open('./data.json') as data_file:
+
+        with open("./data.json") as data_file:
             data = json.load(data_file)
         return data["account_infos"]
+
     def search(self):
         print(self.account_name.get(), type(self.account_name.get()))
+
 
 windows = Tk()
 interface = Interface(windows)
--- writting_data.py
+++ writting_data.py
@@ -1,18 +1,22 @@
 # -*-coding:Utf-8 -*
 import json
+
 data = {}
 data["unlock_password"] = "1234"
-data["ubi"] = {"id": "mon_id","password": "password$£"}
-data["steam"] = {"id": "mon_id","password": "password"}
-with open('data.json', 'w') as outfile:
-    json.dump(data,outfile, ensure_ascii=False, indent=4)
+data["account_infos"] = {
+    "ubi": {"id": "mon_id", "password": "password$£"},
+    "steam": {"id": "mon_id", "password": "password"},
+}
+with open("data.json", "w") as outfile:
+    json.dump(data, outfile, ensure_ascii=False, indent=4)
+
 
 class Password_data:
     def __init__(self):
-        self.data={}
-    
-    def add_new_account(self, account:str, id:str, password:str):
+        self.data = {}
+
+    def add_new_account(self, account: str, id: str, password: str):
         self.data[account] = {"id": id, "password": password}
-        
-    def edit_account(self, account:str, id:str= "", password:str=""):
+
+    def edit_account(self, account: str, id: str = "", password: str = ""):
         print("ok")
